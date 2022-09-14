dd6b8d943c0ab61bb221a20bb79a8bc910e0d8a9
Kelly901 <3416152852103@ingenieria.usac.edu.gt>
2022-09-14 17:35:53 -0600

    nav css

--- views/template.php
+++ views/template.php
@@ -5,9 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Template</title>
+    
+    <link href="views/styles/nav.css" type="text/css" rel="stylesheet">
 </head>
     <body>
 
+   
         <!--Header-->
         <?php include "views/modules/header.php"; ?>
     
