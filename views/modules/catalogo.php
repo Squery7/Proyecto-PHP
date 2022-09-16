34d880983b3054524dfe9ee3d2329e96193b90cd
Kelly901 <3416152852103@ingenieria.usac.edu.gt>
2022-09-15 20:49:53 -0600

    card

--- views/modules/catalogo.php
+++ views/modules/catalogo.php
@@ -1,1 +1,16 @@
 <h1>PÁGINA DE CATÁLOGO</h1>
+
+
+<div id="catalogo" class="catalogo">
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+</div>
+
