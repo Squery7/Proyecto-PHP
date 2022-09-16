6b679ae5a86f126c478fbd3779785a0ea2d272e8
Squery7 <diegomrza98@gmail.com>
2022-09-16 16:55:41 -0600

    For en el catàlogo

--- views/modules/catalogo.php
+++ views/modules/catalogo.php
@@ -3,45 +3,29 @@
 
 <div id="catalogo" class="catalogo">
 
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary" id="btn-primary">Go somewhere</a>
-  </div>
-</div>
+  <?php
+  for ($i=0; $i < 4; $i++) { 
+    $variable = "";
+    $variable = $variable .  '<div class="card" style="width: 18rem;">';
+    $variable = $variable . '  <img class="card-img-top" src="..." alt="Card image cap">';
+    $variable = $variable . '  <div class="card-body">';
+    $variable = $variable . '    <h5 class="card-title">Producto 1</h5>';
+    $variable = $variable . '    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>';
+    $variable = $variable . '    <a href="#" class="btn btn-primary" id="btn-primary">Go somewhere</a>';
+    $variable = $variable . '  </div>';
+    $variable = $variable . '</div>';
+    echo $variable;   
+  }
+  ?>
 
-
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary" id="btn-primary">Go somewhere</a>
-  </div>
-</div>
-
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary" id="btn-primary">Go somewhere</a>
-  </div>
-</div>
-
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary" id="btn-primary">Go somewhere</a>
-  </div>
-</div>
-
-
-
+  <!-- <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Producto 1</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary" id="btn-primary">Go somewhere</a>
+    </div>
+  </div> -->
 
 </div>
 
